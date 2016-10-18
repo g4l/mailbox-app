5b7bf4cf46a950d6cd903ac1e652dc7759a48299
vadim.merkotan <vadim.merkotan@silverlinecrm.com>
2016-10-18 13:35:31 +0300

    change http protocol for service

--- src/services.js
+++ src/services.js
@@ -4,34 +4,34 @@ angular.module('mailbox')
 	let mailboxes;
   this.getAllMails = () => {
 	  if(!mails) {		
-		mails = $http.get('http://test-api.javascript.ru/v1/vmerkotan/letters?delay=1000')
+		mails = $http.get('//test-api.javascript.ru/v1/vmerkotan/letters?delay=1000')
                   .then(response => response.data);
 	  }
       return mails;
     }
   this.getAllMailboxes = () => {
 	  if(!mailboxes) {
-		mailboxes = $http.get('http://test-api.javascript.ru/v1/vmerkotan/mailboxes?delay=1000')
+		mailboxes = $http.get('//test-api.javascript.ru/v1/vmerkotan/mailboxes?delay=1000')
 						.then(response => response.data)
 	  }
       return mailboxes;
     }
    this.deleteMail = letterId => {
-	return $http.delete('http://test-api.javascript.ru/v1/vmerkotan/letters/' + letterId + '?delay=1000');	
+	return $http.delete('//test-api.javascript.ru/v1/vmerkotan/letters/' + letterId + '?delay=1000');	
    }
    
    this.moveToTrash = (letterId, letter) => {
-	return $http.patch('http://test-api.javascript.ru/v1/vmerkotan/letters/' + letterId + '?delay=1000', letter);	
+	return $http.patch('//test-api.javascript.ru/v1/vmerkotan/letters/' + letterId + '?delay=1000', letter);	
    }
    
    this.saveLetter = (mail) => {
 		mails = null;
-		return $http.post('http://test-api.javascript.ru/v1/vmerkotan/letters/', mail);                  
+		return $http.post('//test-api.javascript.ru/v1/vmerkotan/letters/', mail);                  
 	}
    
    //Можно ли так делать, чтобы данные всегда были актуальные?
    $interval(function() {
-	mails = $http.get('http://test-api.javascript.ru/v1/vmerkotan/letters?delay=1000')
+	mails = $http.get('//test-api.javascript.ru/v1/vmerkotan/letters?delay=1000')
                   .then(response => response.data);
    }, 5000);
 })
