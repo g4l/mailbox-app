ec0d36664b007f16b46854fbbba798f3bd82eca6
vadim.merkotan <vadim.merkotan@silverlinecrm.com>
2016-10-20 18:12:11 +0300

    wrapper component on changeStateError change

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,12 +1,10 @@
 angular.module('mailbox')
 	.component('wrapper', {
 		controller: function($scope, $log, $state) {		 
-		 $scope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {				
-				if(toState.name === 'mailboxes') {
-					this.authError = error;
-					$state.go('login');
-				}
-			});
+		 $scope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {					
+			this.authError = error;
+			$state.go('login');
+		});
 			
 		 $scope.$on('$stateChangeSuccess',  (event, toState, toParams, fromState, fromParams, error) => {								
 			    //will hide error when any state was changed successfully
