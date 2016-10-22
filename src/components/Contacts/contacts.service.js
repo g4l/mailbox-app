b80debf23263b2f9f20e7d65a2353e6b0162c4d0
vad1m198 <Persidskij_on_ferrari@ukr.net>
2016-10-22 11:24:26 +0300

    change services

--- src/components/Contacts/contacts.service.js
+++ src/components/Contacts/contacts.service.js
@@ -4,16 +4,16 @@ class ContactsDataSvc {
 	}
 	
 	getAllContacts() {	  	
-		return this.$http.get('http://test-api.javascript.ru/v1/vmerkotan/users?delay=1000')
+		return this.$http.get('//test-api.javascript.ru/v1/vmerkotan/users?delay=1000')
                   .then(response => response.data);	  
     }
 	
 	deleteContact(contactId) {
-		return this.$http.delete('http://test-api.javascript.ru/v1/vmerkotan/users/' + contactId + '?delay=1000');	
+		return this.$http.delete('//test-api.javascript.ru/v1/vmerkotan/users/' + contactId + '?delay=1000');	
    }
    
    createContact(contact) {		
-		return this.$http.post('http://test-api.javascript.ru/v1/vmerkotan/users/?delay=1000', contact)
+		return this.$http.post('//test-api.javascript.ru/v1/vmerkotan/users/?delay=1000', contact)
 					.then(response => response.data);	
 	}
 }
